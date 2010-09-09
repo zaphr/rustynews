96584b6014be6c205c31c90194c3d6a0d48685da
zaphr <areinbach@gmail.com>
2010-09-09 21:29:36 +1000

    Initial setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rustynews::Application.config.secret_token = '47ecb783c53e040bcf1283a4a4067c71f385d7df22038409e2c45ce86edb0d6b69317c765990ea2138326a728543ca50014acdad822d77b73dea5da727a74fca'
