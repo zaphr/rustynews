55880a9a6c99543d1fe8b3f7d4a47b42f6842990
zaphr <areinbach@gmail.com>
2010-09-12 17:29:25 +1000

    Looking funkygit s! New background image

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,50 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+$(document).ready(function(){
+
+//Larger thumbnail preview
+
+$("#article_image img").hover(function() {
+	$(this).css({'z-index' : '10'});
+	$(this).find('img').addClass("hover").stop()
+		.animate({
+			marginTop: '-110px',
+			marginLeft: '-110px',
+			top: '50%',
+			left: '50%',
+			width: '174px',
+			height: '174px',
+			padding: '20px'
+
+		}, 200);
+
+	} , function() {
+	$(this).css({'z-index' : '0'});
+	$(this).find('img').removeClass("hover").stop()
+		.animate({
+			marginTop: '0',
+			marginLeft: '0',
+			top: '0',
+			left: '0',
+			width: '100px',
+			height: '100px',
+			padding: '5px'
+		}, 400);
+});
+
+//Swap Image on Click
+	$("ul.thumb li a").click(function() {
+
+		var mainImage = $(this).attr("href"); //Find Image Name
+		$("#main_view img").attr({ src: mainImage });
+		return false;
+	});
+
+});
+
+$(document).ready(function(){
+
+  if (!$.browser.msie) {
+    $(".round").corner("round  10px");
+    $("#header").corner("round br  10px");
+    $("#top_pagination").corner("round tl tr 10px");
+    }
+});
