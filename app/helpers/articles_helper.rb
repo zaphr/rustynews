2cac9f5136e5bc9385f6b0e156148c8546206d04
zaphr <areinbach@gmail.com>
2010-09-14 23:03:12 +1000

    Load more articles

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,13 @@
 module ArticlesHelper
+
+  def random_article_id_excluding(id)
+    # Create new array as cache returns a frozen array
+    all_ids = Array.new(Article.all_ids_cached)
+    all_ids.delete(id)
+    all_ids.rand
+  end
+
+
+
+
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,17 @@
 class Article < ActiveRecord::Base
+
+  after_save :empty_cache
+  after_destroy :empty_cache
+
+
+  def self.all_ids_cached
+    Rails.cache.fetch('Article.all_ids') {all.collect{|article| article.id}}
+  end
+
+  private
+
+  def empty_cache
+    Rails.cache.delete('Article.all_ids')
+  end
+
 end
