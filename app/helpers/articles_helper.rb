8702b5ab85d218ca44f4db1f52460aeb85b11d26
zaphr <areinbach@gmail.com>
2010-09-17 11:43:47 +1000

    Looking ok, Next Report link, but about to yank background images..

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -7,7 +7,15 @@ module ArticlesHelper
     all_ids.rand
   end
 
-
+  def next_id_in_collection(collection, current_id)
+    collection = Article.all_ids_cached
+    current_found = false
+    collection.each do |article_id|
+      return article_id if current_found
+      current_found = true if article_id == current_id
+    end
+    collection.first
+  end
 
 
 end
